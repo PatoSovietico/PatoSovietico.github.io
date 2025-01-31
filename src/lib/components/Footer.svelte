c1781b8988303dd8fa4dae8feb86787d6053c8e8
PatoSovietico <arthurbat120907@gmail.com>
2025-01-31 00:39:56 -0400

    cu

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>
+    <p>&copy; 2023 Meu Site SvelteKit. Todos os direitos reservados.</p>
+  </footer>
+  
+  <style>
+    footer {
+      background-color: var(--footer-bg);
+      padding: 1rem;
+      text-align: center;
+    }
+  </style>
