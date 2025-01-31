037321ec61aa46d0e89f26864df55d96f4306ddf
PatoSovietico <arthurbat120907@gmail.com>
2025-01-31 00:41:50 -0400

    cu

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Header from '$lib/components/Header.svelte';
-    import Footer from '$lib/components/Footer.svelte';
-    import '../lib/styles/global.css';
-  </script>
-  
-  <Header />
-  <slot /> <!-- Conteúdo das páginas será injetado aqui -->
-  <Footer />
